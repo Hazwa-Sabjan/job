89d5049129f778ed345c5a39326d89a4e9b5243c
amida <amidasubhana@gmail.com>
2025-06-18 06:40:57 +0530

    third commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React, { useContext, useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import ApplyJob from './pages/ApplyJob'
+import Applications from './pages/Applications'
+import RecruiterLogin from './components/RecruiterLogin'
+import { use } from 'react'
+import { AppContext } from './context/AppContext'
+
+const App = () => {
+
+ const {showRecruiterLogin} = useContext(AppContext)
+
+  return (
+    <div>
+   
+        { showRecruiterLogin && <RecruiterLogin />}
+      <Routes>
+        <Route path='/' element={<Home />} />
+         <Route path='/apply-job/:id' element={<ApplyJob />} />
+          <Route path='/applications' element={<Applications />} />
+
+        
+      </Routes>
+    </div>
+  )
+}
+
+export default App
